653d998beb75b1d94b7478295037f2fc0500ecd0
StephenDodd <sdoddaws@gmail.com>
2018-08-09 02:44:39 -0500

    Add project page

--- server.js
+++ server.js
@@ -43,6 +43,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page'
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
